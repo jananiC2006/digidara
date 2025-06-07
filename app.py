35c5c80f021ef77fb6dfb5363858c8ae5ec1875a
jananiC2006 <jananichandran0603@gmail.com>
2025-06-07 11:33:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,79 +1,165 @@
-from flask import Flask, render_template, request,redirect
-from flask_mysqldb import MySQL
+# from flask import Flask, render_template, request,redirect
+# from flask_mysqldb import MySQL
+# import mysql.connector
+
+# app = Flask(__name__)
+# db=mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="",
+#     database="alumni_db"
+# )
+# cursor=db.cursor()
+
+# mysql=MySQL(app)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+# @app.route('/register')
+# def register():
+#     return render_template('register_alumni.html')
+
+# @app.route('/register_alumni', methods=['GET', 'POST'])
+# def register_alumni():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         batch = request.form['batch']
+#         department = request.form['department']
+#         company = request.form['company']
+#         position = request.form['position']
+#         linkedin_url = request.form['linkedin_url']
+
+
+#         cursor.execute('''INSERT INTO alumni(name, email, batch, department, company, position, linkedin_url)
+#             VALUES (%s, %s, %s, %s, %s, %s, %s)
+#         ''', (name, email, batch, department, company, position, linkedin_url))
+#         db.commit()
+        
+
+#         return redirect('/alumni')  
+
+
+# @app.route('/alumni')
+# def alumni_list():
+#     # db=mysql.connector.connect(
+#     #         host="localhost",
+#     #         user="root",
+#     #         password="",
+#     #         database="alumni_db"
+#     #     )
+#     # cursor=db.cursor()
+    
+#     cursor.execute("SELECT * FROM alumni_db WHERE status = 'Approved' ORDER BY submitted_on DESC")
+#     alumni = cursor.fetchall()
+    
+#     return render_template('alumni_list.html',alumni=alumni)
+
+# @app.route('/admin_alumni', methods=['GET', 'POST'])
+# def admin_alumni():
+#     cursor=db.cursor()
+#     if request.method =='POST':
+#         alumni_id = request.form['alumni_id']
+#         status = request.form['status']
+    
+#         cursor.execute("UPDATE alumni SET status = %s WHERE alumni_id = %s", (status, alumni_id))
+#         db.commit()
+   
+#     cursor.execute("SELECT * FROM alumni ORDER BY submitted_on DESC")
+#     data = cursor.fetchall()
+#     cursor.close()
+    
+#     alumni=[]
+#     for row in data:
+#         alumni.append({'name':row[0],'batch':row[1],'department':row[2],'company':row[3],'position':row[4],'linkedin_url':row[5],'status':row[6],'action':row[7]})
+#         return render_template('admin_alumni.html',alumni=alumni)
+    
+
+# @app.route('/thank')
+# def thank():
+#     return render_template('thank.html')
+      
+    
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 
 app = Flask(__name__)
 
-db=mysql.connector.connect(
+db = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
     database="alumni_db"
 )
-cursor=db.cursor()
-
-mysql=MySQL(app)
+cursor = db.cursor()
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/register')
 def register():
     return render_template('register_alumni.html')
 
-@app.route('/register_alumni', methods=['GET', 'POST'])
+@app.route('/register_alumni', methods=['POST'])
 def register_alumni():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        batch = request.form['batch']
-        department = request.form['department']
-        company = request.form['company']
-        position = request.form['position']
-        linkedin_url = request.form['linkedin_url']
+    name = request.form['name']
+    email = request.form['email']
+    batch = request.form['batch']
+    department = request.form['department']
+    company = request.form['company']
+    position = request.form['position']
+    linkedin_url = request.form['linkedin_url']
 
+    cursor.execute('''
+        INSERT INTO alumni(name, email, batch, department, company, position, linkedin_url)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ''', (name, email, batch, department, company, position, linkedin_url))
+    db.commit()
 
-        cursor.execute('''INSERT INTO alumni_db (name, email, batch, department, company, position, linkedin_url)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-        ''', (name, email, batch, department, company, position, linkedin_url))
-        db.commit()
-        
-
-        return redirect('/alumni')  
-
+    return redirect('/alumni')
 
 @app.route('/alumni')
 def alumni_list():
-    # db=mysql.connector.connect(
-    #         host="localhost",
-    #         user="root",
-    #         password="",
-    #         database="alumni_db"
-    #     )
-    # cursor=db.cursor()
-    
-    cursor.execute("SELECT * FROM alumni_db WHERE status = 'Approved' ORDER BY submitted_on DESC")
+    cursor.execute("SELECT * FROM alumni WHERE status = 'Approved' ORDER BY submitted_on DESC")
     alumni = cursor.fetchall()
-    
-    return render_template('alumni_list.html',alumni=alumni)
+    return render_template('alumni_list.html', alumni=alumni)
 
-@app.route('/admin', methods=['GET', 'POST'])
-def admin_approval():
-    if request.method =='POST':
+@app.route('/admin_alumni', methods=['GET', 'POST'])
+def admin_alumni():
+    if request.method == 'POST':
         alumni_id = request.form['alumni_id']
         status = request.form['status']
-
-        cursor = db.connection.cursor()
         cursor.execute("UPDATE alumni SET status = %s WHERE alumni_id = %s", (status, alumni_id))
-        db.connection.commit()
-        cursor=db.cursor()
-        
-   
+        db.commit()
+
     cursor.execute("SELECT * FROM alumni ORDER BY submitted_on DESC")
-    alumni = cursor.fetchall()
-    cursor.close()
-    
+    data = cursor.fetchall()
+
+    alumni = []
+    for row in data:
+        alumni.append({
+            'alumni_id': row[0],
+            'name': row[1],
+            'email': row[2],
+            'batch': row[3],
+            'department': row[4],
+            'company': row[5],
+            'position': row[6],
+            'linkedin_url': row[7],
+            'status': row[8]
+        })
+
     return render_template('admin_alumni.html', alumni=alumni)
+
+@app.route('/thank')
+def thank():
+    return render_template('thank.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
